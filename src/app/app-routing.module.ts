3217a565d38c739805dc54e04614e047322f5d96
Gilberto Hdez <gilhdez79@yahoo.com.mx>
2023-02-02 12:45:09 -0600

    UPDATE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,17 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, CanActivate,Router } from '@angular/router';
 import { ArticulosComponent } from './components/articulos/articulos.component';
 import { TiendasComponent } from './components/tiendas/tiendas.component';
 import { ListaArticulosComponent } from './components/lista-articulos/lista-articulos.component';
-import { LoginComponent } from './components/login/login.component';
-import { AuthGuardsGuard } from './auth/auth-guards.guard';
-
+import { SeguridadGuard } from './seguridad.guard';
 
 const routes: Routes = [
-  {path:'', component:LoginComponent},
-  {path:'login', component:LoginComponent,
-    canActivate:[AuthGuardsGuard]},
-  {path:'articulos', component:ArticulosComponent,
-  canActivate:[AuthGuardsGuard]},
-  {path:'tiendas', component:TiendasComponent,
-  canActivate:[AuthGuardsGuard]},
-  {path:'lista-articulos', component:ListaArticulosComponent,
-  canActivate:[AuthGuardsGuard]},
+  { path:'articulos',
+   component:ArticulosComponent,
+   CanActivate: [SeguridadGuard]
+  },
+  {path:'tiendas', component:TiendasComponent},
+  {path:'lista-articulos', component:ListaArticulosComponent},
 
 
 ];
